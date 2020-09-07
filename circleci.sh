dda9fd0d941a70ae29b9a57ec604088dd4d80faf
bschuedzig <benjamin.schuedzig@mantro.net>
2020-09-07 16:48:01 +0200

    Beautifies CircleCI workflow

--- circleci.sh
+++ circleci.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-apk add --no-cache shellcheck
-
 cd /root/bashops
 cd src
 shellcheck -x ./*
--- simulate-circleci.sh
+++ simulate-circleci.sh
@@ -6,4 +6,4 @@ GITROOT="$(git rev-parse --show-toplevel)"
 docker run \
     -v "${GITROOT}:/root/bashops" \
     alpine:3.12.0 \
-    /bin/sh -c "apk add bash && cd /root/bashops && /bin/bash ./circleci.sh"
+    /bin/sh -c "apk add bash shellcheck && cd /root/bashops && /bin/bash ./circleci.sh"
